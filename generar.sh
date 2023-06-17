9e263e0d4aad4546c792d9a9045e26869baf1aad
nataniel <natanielpezotto7@gmail.com>
2023-06-16 21:57:19 -0300

    Add generar.sh

---
+++ generar.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+#lista_nombres = ("Juan" "María" "Pedro" "Laura" "Carlos")
+
+#echo "Ingrese cantidad de imagenes a descargar"
+#read cantidad_imagenes
+
+
+for i in {seq 1 $1}
+
+do
+    nombre=$(sed -n"$RANDOM"p array)
+    echo $nombre
+    curl -L https://source.unsplash.com/random/900%C3%97700/?person --output $RANDOM
+    sleep 1
+done
+
+
