98ef7f69d55851334d1d79e0e2bef110ecd70b1c
ssarara <ssarara@hotmail.com>
2023-06-30 21:45:17 -0300

    add name to downloaded images

--- generar.sh
+++ generar.sh
@@ -1,16 +1,26 @@
 
 #!/bin/bash
 
-#lista_nombres = ("Juan" "María" "Pedro" "Laura" "Carlos")
+#AGREGAR FUNCION PARA CONSEGUIR EL NOMBRE Y METERLO ADENTRO DEL FOR
+#CREAR Y GUARDAR LAS IMAGENES EN LA CARPETA IMAGENES EN LA LINEA DEL CURL
+#IGNORAR EL CSV Y TRABAJARLO DESCARGANDOLO. VALIDAR QUE EXISTA O NO EL ARCHIVO
 
-#echo "Ingrese cantidad de imagenes a descargar"
-#read cantidad_imagenes
+TOTAL_LINES=$(wc -l < ./dict.csv)
+RANDOM_LINE=$((RANDOM%$TOTAL_LINES+ 1))
+
+i=0
+while read line; do
+        i=$(( i + 1 ))
+	case $i in $RANDOM_LINE)break; esac
+done < "./dict.csv"
+
+
+NOMBRE=$(echo $line | cut -d , -f 1)
+echo $NOMBRE
 
 for i in $(seq 0 $(($1 - 1 )))
 do
-    nombre=$(sed -n"$RANDOM"p array)
-    echo $nombre
-    curl -L https://source.unsplash.com/random/900%C3%97700/?person --output $RANDOM
+    curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$NOMBRE")
     sleep 1
 done
 
