0f28660fb6d10137fd08e5b1ae6d52b91e9980f4
sofiapel <115552371+BrunoDavid1@users.noreply.github.com>
2023-07-09 21:15:10 -0300

    add compression

--- generar.sh
+++ generar.sh
@@ -5,23 +5,19 @@
 #CREAR Y GUARDAR LAS IMAGENES EN LA CARPETA IMAGENES EN LA LINEA DEL CURL
 #IGNORAR EL CSV Y TRABAJARLO DESCARGANDOLO. VALIDAR QUE EXISTA O NO EL ARCHIVO
 
-TOTAL_LINES=$(wc -l < ./dict.csv)
-RANDOM_LINE=$((RANDOM%$TOTAL_LINES+ 1))
-
-i=0
-while read line; do
-        i=$(( i + 1 ))
-	case $i in $RANDOM_LINE)break; esac
-done < "./dict.csv"
-
-
-NOMBRE=$(echo $line | cut -d , -f 1)
-echo $NOMBRE
+mkdir -p images 
+cd ./images
 
 for i in $(seq 0 $(($1 - 1 )))
 do
+	LINE=$(shuf -n 1 ../dict.csv) 
+	echo $LINE
+
+	NOMBRE=$(echo $LINE | cut -d , -f 1)
     curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$NOMBRE"
     sleep 1
 done
 
+cd ..
 
+tar zcvf images.tar.gz images
