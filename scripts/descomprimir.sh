f9c424a7d0ace730b13431a614bae8bc646282fc
sofiapel <sofiacarlapellegrini@gmail.com>
2023-07-11 23:10:26 -0300

    create descomprimir.sh

---
+++ scripts/descomprimir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IMAGES=$1
+CHECK_SUM=$2
+[[ ! -f $IMAGES || ! -f $CHECK_SUM ]] && echo "Debes ingresar dos archivos." && exit 0
+CORRECT_SUM=$(cat $CHECK_SUM)
+SUM=$(md5sum $IMAGES | awk '{print $1}')
+
+#[[ "$CORRECT_SUM" == "$SUM" ]] && echo "Suma de verificacion realizada correctamente!" || "Archivo a descomprimir incorrecto" && exit 0
+
+[[ "$CORRECT_SUM" == "$SUM" ]] && echo "Suma de verificación realizada correctamente!" || { echo "Archivo a descomprimir incorrecto"; exit 0; }
+
+
+tar -xvf $IMAGES -C ../
+
+echo "Descompresion de imagenes realizada correctamente!"
