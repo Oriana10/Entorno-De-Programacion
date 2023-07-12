28768b00be61e1ab2b47eab4943115d14de9e4da
sofiapel <sofiacarlapellegrini@gmail.com>
2023-07-11 23:09:58 -0300

    add validations and delete images folder

--- scripts/generar.sh
+++ scripts/generar.sh
@@ -4,6 +4,10 @@
 #AGREGAR FUNCION PARA CONSEGUIR EL NOMBRE Y METERLO ADENTRO DEL FOR
 #CREAR Y GUARDAR LAS IMAGENES EN LA CARPETA IMAGENES EN LA LINEA DEL CURL
 #IGNORAR EL CSV Y TRABAJARLO DESCARGANDOLO. VALIDAR QUE EXISTA O NO EL ARCHIVO
+
+[[ $# -ne 1 ]] && echo "Debes ingresar un solo argumento" && exit 0
+! [[ '^[0-9]+$' =~ $1 ]] && echo "El argumento debe ser un numero" && exit 0
+
 cd ..
 mkdir -p images 
 cd ./images
@@ -11,11 +15,10 @@ cd ./images
 for i in $(seq 0 $(($1 - 1 )))
 do
 	LINE=$(shuf -n 1 ../dict.csv) 
-	echo $LINE
 
 	NOMBRE=$(echo $LINE | cut -d , -f 1)
-    curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$NOMBRE"
-    sleep 1
+    	curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$NOMBRE"
+    	sleep 1
 done
 
 cd ..
@@ -27,4 +30,6 @@ checksum=$(md5sum images.tar.gz | awk '{print $1}')
 # Genera el archivo con el checksum
 echo "$checksum" > checksum.txt
 
-echo "Generación de imágenes completada."
+echo "Generación y compresion de imágenes completada."
+
+rm -r images
