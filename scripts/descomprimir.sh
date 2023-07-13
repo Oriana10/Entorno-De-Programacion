8d77066e1dddf3824a0ffaf9663855643ebb2cb0
sofiapel <sofiacarlapellegrini@gmail.com>
2023-07-13 15:13:37 -0300

    update validations

--- scripts/descomprimir.sh
+++ scripts/descomprimir.sh
@@ -2,13 +2,13 @@
 
 IMAGES=$1
 CHECK_SUM=$2
-[[ ! -f $IMAGES || ! -f $CHECK_SUM ]] && echo "Debes ingresar dos archivos." && exit 0
+[[ ! -f $IMAGES || ! -f $CHECK_SUM ]] && echo "Debes ingresar dos archivos." && exit 1
 CORRECT_SUM=$(cat $CHECK_SUM)
 SUM=$(md5sum $IMAGES | awk '{print $1}')
 
 #[[ "$CORRECT_SUM" == "$SUM" ]] && echo "Suma de verificacion realizada correctamente!" || "Archivo a descomprimir incorrecto" && exit 0
 
-[[ "$CORRECT_SUM" == "$SUM" ]] && echo "Suma de verificación realizada correctamente!" || { echo "Archivo a descomprimir incorrecto"; exit 0; }
+[[ "$CORRECT_SUM" == "$SUM" ]] && echo "Suma de verificación realizada correctamente!" || { echo "Archivo a descomprimir incorrecto"; exit 1; }
 
 
 tar -xvf $IMAGES -C ../
